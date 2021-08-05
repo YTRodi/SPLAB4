d0c7b33f4d2661bc64284acdd2f47a5873f8691b
YTRodi <yagorodi@gmail.com>
2021-08-05 18:50:53 -0300

    typo fixed

--- src/app/protected/screen/admin/list-users/list-users.component.ts
+++ src/app/protected/screen/admin/list-users/list-users.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
-  styleUrls: ['./list-users.component.css']
+  styleUrls: ['./list-users.component.css'],
 })
 export class ListUsersComponent implements OnInit {
+  public selectedFilter: 'STUDENT' | 'TEACHER' | 'ADMIN' | 'ALL' = 'ALL';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  setSelectedFilter(value: 'STUDENT' | 'TEACHER' | 'ADMIN' | 'ALL') {
+    this.selectedFilter = value;
   }
-
 }
