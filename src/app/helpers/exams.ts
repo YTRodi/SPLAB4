d90726218ab49d2901110826cdd7790953d1285d
YTRodi <yagorodi@gmail.com>
2021-08-05 15:00:57 -0300

    add date field in exam table component

--- src/app/helpers/exams.ts
+++ src/app/helpers/exams.ts
@@ -1,5 +1,17 @@
 import { Exam } from '../interfaces/exam.interface';
 
+// export const groupExamsBySubjectNames = (items: Exam[]) => {
+//   return items.reduce((acc: any, cur: any) => {
+//     if (acc[cur.subject.name]) {
+//       acc[cur.subject.name].push(cur);
+//     } else {
+//       acc[cur.subject.name] = [cur];
+//     }
+
+//     return acc;
+//   }, {});
+// };
+
 export const groupExamsBySubjectNames = (items: Exam[]) => {
   return items.reduce((acc: any, cur: any) => {
     if (acc[cur.subject.name]) {
